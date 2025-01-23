005d9ece865475b580f30ec90a3bb789c924ada4
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-01-23 18:58:59 +0400

    change regexvalidator

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -4,8 +4,11 @@ from django.core.validators import RegexValidator
 
 def validate_username(username):
     """Валидация имени пользователя"""
-    if not RegexValidator(r'^[\w.@+-]+\z')(username):
-        raise ValidationError(
-            'Имя пользователя может содержать только буквы, \
-                цифры и символы @/./+/-/_.'
+    validator = RegexValidator(
+        r'^[\w.@+-]+$',
+        message=(
+            "Имя пользователя может содержать только буквы, "
+            "цифры и символы @/./+/-/_."
         )
+    )
+    validator(username)

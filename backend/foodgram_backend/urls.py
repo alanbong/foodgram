cf997c128b1186a5e77374898e018d9e126a8f99
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-01-23 18:58:44 +0400

    del redoc from main url

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
+
 from django.conf import settings
 from django.conf.urls.static import static
-from foodgram_backend.settings import BASE_DIR
 
-DOCS_DIR = BASE_DIR.parent / 'docs'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        serve,
-        {'path': 'redoc.html', 'document_root': DOCS_DIR},
-        name='redoc'
-    ),
 ]
 
 if settings.DEBUG:

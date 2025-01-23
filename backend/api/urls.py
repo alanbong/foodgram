50bc6ae3f9512c455b9cc689671aa274a93a97bd
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-01-23 18:57:11 +0400

    add redoc, djoser and users urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,29 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.static import serve
+from rest_framework.routers import DefaultRouter
 
+from foodgram_backend.settings import BASE_DIR
+from .views import (
+    UsersViewSet,
+)
+
+DOCS_DIR = BASE_DIR.parent / 'docs'
+
+router = DefaultRouter()
+router.register(r'users', UsersViewSet, basename='users')
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),
+    path(
+        'redoc/', serve,
+        {'path': 'redoc.html', 'document_root': DOCS_DIR},
+        name='redoc'
+    ),
+    path(
+        'redoc/openapi-schema.yml', serve,
+        {'path': 'openapi-schema.yml', 'document_root': DOCS_DIR},
+        name='redoc'
+    ),
 ]

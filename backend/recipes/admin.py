14417eb4bd47d9e364e54a721335d33c175a4ee9
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-01-31 14:35:12 +0400

    regiser tag and ingredient in admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "measurement_unit")
+    search_fields = ("name",)

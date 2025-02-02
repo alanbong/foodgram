abb2c64113d516d8a300fe8e0d75d442bafa7281
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-02-02 18:52:46 +0400

    change permissions logic

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,10 +1,11 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, SAFE_METHODS
 
 
-class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
+class IsSelfOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
     """
-    Доступ для чтения всем, а для редактирования автору,
-    модератору или администратору.
+    Разрешение для работы с профилями пользователей.
+    - Доступ для чтения всем
+    - Доступ для редактирования администратору и владельцу профиля.
     """
 
     def has_object_permission(self, request, view, obj):
@@ -14,5 +15,20 @@ class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
         return (
             request.user.is_admin
             or obj.id == request.user
+        )
+
+class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
+    """
+    Разрешение для работы с рецептами.
+    - Доступ для чтения всем
+    - Доступ для редактирования администратору и автору.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return (
+            request.user.is_admin
             or obj.author == request.user
         )

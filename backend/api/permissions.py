42b69ed076ca49a2b39b909ecaf75dc5ece3c351
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-02-06 22:53:00 +0400

    add custom permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,4 +1,6 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, SAFE_METHODS
+from rest_framework.permissions import (
+    BasePermission, SAFE_METHODS, IsAuthenticatedOrReadOnly
+)
 
 
 class IsSelfOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
@@ -14,14 +16,16 @@ class IsSelfOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
 
         return (
             request.user.is_admin
+            or request.user.is_staff
             or obj.id == request.user
         )
+
 
 class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
     """
     Разрешение для работы с рецептами.
     - Доступ для чтения всем
-    - Доступ для редактирования администратору и автору.
+    - Доступ для редактирования, удалению администратору и автору.
     """
 
     def has_object_permission(self, request, view, obj):
@@ -30,5 +34,21 @@ class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
 
         return (
             request.user.is_admin
+            or request.user.is_staff
             or obj.author == request.user
+        )
+
+
+class IsAdmin(BasePermission):
+    """
+    Проверка, является ли пользователь администратором.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.is_admin
+                or request.user.is_staff
+            )
         )

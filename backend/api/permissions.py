348eff74aa460c1abc16fdd7569af5fccc5c24f4
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-02-09 23:34:28 +0400

    del unused permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,54 +1,17 @@
 from rest_framework.permissions import (
-    BasePermission, SAFE_METHODS, IsAuthenticatedOrReadOnly
+    SAFE_METHODS, IsAuthenticatedOrReadOnly
 )
 
 
-class IsSelfOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
-    """
-    Разрешение для работы с профилями пользователей.
-    - Доступ для чтения всем
-    - Доступ для редактирования администратору и владельцу профиля.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return (
-            request.user.is_admin
-            or request.user.is_staff
-            or obj.id == request.user
-        )
-
-
-class IsOwnerOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
+class IsOwnerOrReadOnly(IsAuthenticatedOrReadOnly):
     """
     Разрешение для работы с рецептами.
     - Доступ для чтения всем
-    - Доступ для редактирования, удалению администратору и автору.
+    - Доступ для редактирования, удалению автору.
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-
         return (
-            request.user.is_admin
-            or request.user.is_staff
+            request.method in SAFE_METHODS
             or obj.author == request.user
-        )
-
-
-class IsAdmin(BasePermission):
-    """
-    Проверка, является ли пользователь администратором.
-    """
-
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (
-                request.user.is_admin
-                or request.user.is_staff
-            )
         )
